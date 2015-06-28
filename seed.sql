0af1bab6650f76236afa376e3010c6f56122e999
gkbsoftware <gkbsoftware@gmail.com>
2015-06-28 18:34:11 -0400

    fixed seed

--- seed.sql
+++ seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO tweets (body, created_at)
-  VALUES ('Lets use databases to make our apps do things',now());
-INSERT INTO tweets (body, created_at)
-  VALUES ('June is the best month of the year',now());
+INSERT INTO tweets (body, created_at, username)
+  VALUES ('Lets use databases to make our apps do things',now(), 'Greg');
+INSERT INTO tweets (body, created_at, username)
+  VALUES ('June is the best month of the year',now(), 'Nazi');
